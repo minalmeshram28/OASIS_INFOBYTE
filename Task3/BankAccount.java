c95aeb363bfb527f3b782ce5f422307250056097
Minal Meshram <96578677+minalmeshram28@users.noreply.github.com>
2023-11-14 17:08:23 +0530

    Add files via upload

---
+++ Task3/BankAccount.java
@@ -0,0 +1,29 @@
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid amount for deposit.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Invalid amount for withdrawal or insufficient balance.");
+        }
+    }
+}
